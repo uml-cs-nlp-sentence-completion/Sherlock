c857447f74e795d9894bb0395aa59d579b0fa413
Renan Campos <Renan_Campos@student.uml.edu>
2015-11-12 20:39:12 -0500

    Completed a working implementation of the simple n-gram model

--- sherlock/CMU_SLM_NGRAM.sh
+++ sherlock/CMU_SLM_NGRAM.sh
@@ -8,8 +8,14 @@
 # match, and three times for each four-gram match. Where a match means that the
 # test sentence contains the target word at least once in the background data.
 #
+#  > $TMP_DIR/$$.idngram \
+#  && \
+#  echo "here" && \
+#  $CMU_DIR/idngram2lm -idngram $TMP_DIR/$$.idngram -vocab $TMP_DIR/$$.vocab -n 4 \
+#  -binary $TMP_DIR/MSR.binlm \
 
 CMU_DIR="../bin/CMU-Cam_Toolkit_v2/bin"
+TMP_DIR="../tmp"
 
 function print_help() {
   echo "ERROR: invalid arguments"
@@ -21,14 +27,19 @@ function print_help() {
 
 function train_data() {
   # Argument should be path to training data
-  cat $1* | $CMU_DIR/text2wfreq | $CMU_DIR/wfreq2vocab > $$.vocab
-  cat $1* | $CMU_DIR/text2idngram -vocab $$.vocab -n 4 > $$.idngram
-  
-  $CMU_DIR/idngram2lm -idngram $$.idngram -vocab $$.vocab -n 4 -binary MSR.binlm
-
-  rm -f $$.vocab
-  rm -f $$.idngram
+  cat $1* \
+  | $CMU_DIR/text2wfreq \
+  | $CMU_DIR/wfreq2vocab -top 30000 > $TMP_DIR/$$.vocab \
+  && \
+  echo "After freq" && \
+  cat $1* \
+  | $CMU_DIR/text2idngram -n 4 -temp $TMP_DIR/ -vocab $TMP_DIR/$$.vocab \
+  | $CMU_DIR/idngram2lm -n 4 -vocab $TMP_DIR/$$.vocab -idngram - \
+  -binary $TMP_DIR/MSR.binlm -spec_num 10000000 100000000 1000000000 \
+  && \
   echo "Simple 4-gram model trained successfully"
+
+  rm $TMP_DIR/$$.vocab 
 }
 
 function test_data() {
@@ -36,22 +47,25 @@ function test_data() {
   # Using the trained language model, this will evaluate each line of the
   # machine format test file (after removing the question number and brackets
   # using sed), then calculate the score using awk.
-  echo > Simple_ngram.res
+  echo > $TMP_DIR/Simple_ngram.res
   cat $1/Holmes.machine_format.questions.txt | while read line
   do
-    echo $line | sed 's/.*) \|\[\|\]//g' > $$.txt
-    score=$(echo "perplexity -text $$.txt" | $CMU_DIR/evallm -binary MSR.binlm \
-    | grep "Number of" | awk '\
-    BEGIN {score=0; weight=3} \
-    {score = (score + ($6 * weight)); weight--; } \
-    END {print score}')
+    echo $line | sed 's/.*) \|\[\|\]//g' > $TMP_DIR/$$.txt
+    score=$(echo "perplexity -text $TMP_DIR/$$.txt" \
+      | $CMU_DIR/evallm -binary $TMP_DIR/MSR.binlm \
+      | grep "Number of" \
+      | awk '\
+            BEGIN {score=0; weight=3} \
+            {score = (score + ($6 * weight)); weight--; } \
+            END {print score}' \
+    )
 
     echo $score
-    echo $line $score >> Simple_ngram.res
+    echo $line $score >> $TMP_DIR/Simple_ngram.res
    
   done
 
-  rm $$.txt
+  rm $TMP_DIR/$$.txt
 }
 
 function check_dir() {
@@ -83,7 +97,4 @@ else
   print_help $@
   exit 1
 fi
-
-echo $@
-echo $$
 

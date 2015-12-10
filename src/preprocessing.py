e3aa15b76d286e9e6761b937f478e81cf95d2412
Gayas Chowdhury <gucshiblu@yahoo.com>
2015-12-10 00:57:25 -0500

    updated preprocessing
    updated

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -2,6 +2,16 @@
 
 # Author : Gayas Chowdhury
 
+# Summary :
+# This program preprocess raw text corpus for different models
+# It removes all the meta data, and make them ready for feeding into Language models
+# It takes 2 parameters. They are, f/files, and o/outputdir
+# -f/--files    : path to files for preprocessing
+# -o/outputdir  : location for storing preprocessed files
+# Usage :
+# python preprocessing.py -f file01.txt file02.txt -o output_dir
+# python preprocessing.py -f corpus/* -o output_dir
+
 from __future__ import print_function
 from __future__ import unicode_literals
 
@@ -10,12 +20,15 @@ import sys
 import argparse
 import nltk
 import re
+import tools
+import string
 
 from os import path
 from nltk.tokenize import WordPunctTokenizer
 
 end_tags = ["End of Project Gutenberg's Etext", "End of Project Gutenberg Etext", "End of the Project Gutenberg Etext", "End of the Project Gutenberg eText"]
 
+#Remove the metadata
 def preprocess_text(input_path):
     end_tag = "*END*"
     seen_end_tag = False
@@ -48,57 +61,62 @@ def preprocess_text(input_path):
 
     return content
 
-def write_to_file(content, filepath):
-    ofile = open(filepath, 'w')
-    
+#Make file contents as collection of sentences
+def make_sententens(content):
     content     = content.decode('utf8');
     sentences   = nltk.sent_tokenize(content)
     
+    sents = []
     for sent in sentences:
-        sent    = expand_contraction(sent.lower())
-        words   = nltk.word_tokenize(sent)
-        line    = ' '.join(words)
+        sent = process_sentence(sent)
         
-        print(line, file=ofile)
+        if len(sent) > 0:
+            sents.append(sent)
+
+    return sents
+
+def process_sentence(sent):
+    sent    = sent.lower()
+        
+    words   = nltk.word_tokenize(sent)
+    sent    = ' '.join(words)
+    sent    = tools.replace_punct(sent)
+    sent    = tools.replace_number(sent)
+    sent    = ' '.join([w.strip() for w in sent.split() if len(w.strip()) > 0]) #Remove extra spaces
+
+    return sent
+
+
+def format_text(file_content):
+    file_content = file_content.replace('\r\n', ' ')
+    file_content = file_content.replace('\"', ' ')
+    file_content = re.sub(r'\bMr\.|mr\.\b','mr', file_content)
+    file_content = re.sub(r'\bMrs\.|mrs\.\b','mrs', file_content)
+    file_content = re.sub(r'\bDr\.|dr\.\b','dr', file_content)
     
-    ofile.close()
-
-
-def format_text(content):
-    content = content.replace('\r\n', ' ')
-    content = content.replace('"', ' ')
-    content = re.sub(r'\bMr\.|mr\.\b',' ', content)
-    content = re.sub(r'\bMrs\.|mrs\.\b',' ', content)
-    content = re.sub(r'\bDr\.|dr\.\b',' ', content)
-
-    return content
-
-contractions_list = { "ain't" : 'am not', "aren't" : 'are not', "can't" : 'can not', "could've": 'could have', "couldn't":	'could not', "couldn't've":	'could not have', "didn't":	'did not', "doesn't":	'does not', "don't":	'do not', "hadn't":	'had not', "hadn't've":	'had not have', "hasn't":	'has not', "haven't":	'have not', "he'd":	'he had', "he'd've":	'he would have', "he'll":	'he will', "he's":	'he is', "he'sn't":	'he is not', "how'd":	'how would', "how'll":	'how will', "how's":	'how is', "i'd":	'i would', "i'd've":	'i would have', "i'll":	'i will', "i'm":	'i am', "i've":	'i have', "i'ven't":	'i have not', "isn't":	'is not', "it'd":	'it had', "it'd've":	'it would have', "it'll":	'it will', "it's":	'it is', "it'sn't":	'it is not', "let's":	'let us', "ma'am":	'madam', "mightn't":	'might not', "mightn't've":	'might not have', "might've":	'might have', "mustn't":	'must not', "must've":	'must have', "needn't":	'need not', "not've":	'not have', "o'clock":	'of the clock', "oughtn't":	'ought not', "shan't":	'shall not', "she'd":	'she had or she would', "she'd've":	'she would have', "she'll":	'she shall or she will', "she's":	'she has or she is', "she'sn't":	'she has not or she is not', "should've":	'should have', "shouldn't":	'should not', "shouldn't've":	'should not have', "somebody'd":	'somebody had or somebody would', "somebody'd've":	'somebody would have', "somebody'dn't've":	'somebody would not have', "somebody'll":	'somebody shall or somebody will', "somebody's":	'somebody has or somebody is', "someone'd":	'someone had or someone would', "someone'd've":	'someone would have', "someone'll":	'someone shall or someone will', "someone's":	'someone has or someone is', "something'd":	'something had or something would', "something'd've":	'something would have', "something'll":	'something shall or something will', "something's":	'something has or something is', "that'll":	'that will', "that's":	'that has or that is', "there'd":	'there had or there would', "there'd've":	'there would have', "there're":	'there are', "there's":	'there has or there is', "they'd":	'they had or they would', "they'd've":	'they would have', "they'll":	'they shall or they will', "they're":	'they are', "they've":	'they have', "'twas":	'it was', "wasn't":	'was not', "we'd":	'we had or we would', "we'd've":	'we would have', "we'll":	'we will', "we're":	'we are', "we've":	'we have', "weren't":	'were not', "what'll":	'what shall or what will', "what're":	'what are', "what's":	'what has or what is or what does', "what've":	'what have', "when's":	'when has or when is', "where'd":	'where did', "where's":	'where has or where is', "where've":	'where have', "who'd":	'who would or who had', "who'd've":	'who would have', "who'll":	'who shall or who will', "who're":	'who are', "who's":	'who has or who is', "who've":	'who have', "why'll":	'why will', "why're":	'why are', "why's":	'why has or why is', "won't":	'will not', "won't've":	'will not have', "would've":	'would have', "wouldn't":	'would not', "wouldn't've":	'would not have', "y'all":	'you all', "y'all'll":	'you all will', "y'all'd've":	'you all would have', "y'all'dn't've":	'you all would not have', "you'd":	'you had or you would', "you'd've":	'you would have', "you'll":	'you shall or you will', "you're":	'you are', "you'ren't":	'you are not', "you've" :	'you have', "you'ven't": 'you have not'}
-
-
-def expand_contraction(text):
-    text = text.split()
-    words = [contractions_list[word] if word in contractions_list else word for word in text]
-
-    return ' '.join(words)
-
+    return file_content
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--input', dest="input", required=True, nargs="*", help="Input file names")
+    parser.add_argument('-f', '--files', dest="file_names", required=True, nargs="*", help="Input file names")
     parser.add_argument('-o', '--outputdir', dest="output_dir", required=True, nargs=1, help="Output dir")
     
-    args            = parser.parse_args()
-    files           = args.input
+    args        = parser.parse_args()
+    file_names  = args.file_names
+    output_dir  = args.output_dir[0]
     
-    output_dir      = args.output_dir[0]
-
-    for file in files:
-        if path.isfile(file):
-            file_name = path.basename(file)
-            out_file = path.join(output_dir, file_name)
-            print('Processing ' + file)
-            content = preprocess_text(file)
-            content = format_text(content)
-            write_to_file(content, out_file)
+    for file_name in file_names:
+        if path.isfile(file_name):
+            file_basename   = path.basename(file_name)
+            out_file        = path.join(output_dir, file_basename)
+            
+            print('Processing ' + file_name)
+            
+            file_content    = preprocess_text(file_name)
+            file_content    = format_text(file_content)
+            sents           = make_sententens(file_content)
+            
+            tools.write_collection(sents, out_file)
+        else:
+            print(file_name + ' is not found')
 
--- src/tools.py
+++ src/tools.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
-#Author : Gayas Chowdhury
+# Author : Gayas Chowdhury
+# Summary : Utility functions
 
 from __future__ import print_function
 
@@ -9,13 +10,46 @@ import sys
 import argparse
 import nltk
 import re
+import string
 
 from nltk.corpus import stopwords
 from nltk.stem.porter import *
 from os import path
 
-def remove_punctuation(text):
-    return re.sub("[^a-zA-Z]", " ", text)
+EMPTY_LIST      = []
+EMPTY_STRING    = ''
+TAB             = '\t'
+START_TOKEN     = '<s>'
+END_TOKEN       = '</s>'
+SPACE           = ' '
+
+
+#Class for reading sentences from files
+class Sentences(object):
+    def __init__(self, file_names):
+        self.file_names = file_names
+    
+    def __iter__(self):
+        for fname in self.file_names:
+            for line in open(fname):
+                yield line[:-1]
+
+# Remove punctions from text
+def remove_punctuation(text, especial_puncts = EMPTY_LIST):
+    words   = text.split()
+    puncts  = list(string.punctuation) + especial_puncts
+    words   = [w for w in words if w not in puncts]
+    
+    return ' '.join(words)
+
+#Replace punctuation with specified token
+def replace_punct(sent, token = ''):
+    regex   = re.compile('[%s]' % re.escape(string.punctuation))
+    sent    = regex.sub(token, sent)
+    sent    = sent.split()
+    sent    = ' '.join(sent)
+    
+    return sent
 
 #  stem_text(text) : method to remove and replace word suffixes
 #   Parameters:
@@ -30,8 +64,15 @@ def stem_text(text):
 
     return snt
 
+#Replace number with specified token or N
+def replace_number(sentence, token = 'N'):
+    pattern = r"[+-]?[\d\,]+(?:\.\d+)?(?:[eE][+-]?\d+)?" #regex for number
+    
+    return re.sub(pattern, token, sentence)
+
+# Mark the sentence with <s> and </s> tokens
 def mark_sentence(sent):
-    return "<s> " + sent.strip() + " </s>"
+    return START_TOKEN + SPACE + sent.strip() + SPACE + END_TOKEN
 
 # Method : remove_stopwords(text) : remove stop words from the sentences
 #   Parameters:
@@ -47,10 +88,45 @@ def remove_stopwords(text):
     return ' '.join(meaningful_words)
 
 def process_sentence(sentence):
-    sentence = stem_text(remove_punctuation(remove_stopwords(sentence)))
+    sentence = replace_number(remove_punctuation(sentence))
+    
     if len(sentence.strip()) > 0:
         sentence = mark_sentence(sentence)
     
     return sentence
 
+def write_collection(lst, file_name):
+    ofile = open(file_name, 'w')
+    
+    for em in lst:
+        print(em, file=ofile)
+    
+    ofile.close()
 
+# Common Method for formating sentence
+def process_sent_stem(tokenized_sent):
+    sent    = replace_punct(tokenized_sent, EMPTY_STRING)
+    sent    = replace_number(sent)
+    sent    = stem_text(sent)
+    
+    sent = ' '.join([w.strip() for w in sent.split() if len(w.strip()) > 0]) #Remove extra spaces
+    
+    return sent
+
+# Method for formating sentence for Skip-Gram
+def process_sent(tokenized_sent):
+    sent    = replace_punct(tokenized_sent, EMPTY_STRING)
+    sent    = replace_number(sent)
+    
+    sent = ' '.join([w.strip() for w in sent.split() if len(w.strip()) > 0]) #Remove extra spaces
+    
+    return sent
+
+
+contractions_list = { "ain't" : 'am not', "aren't" : 'are not', "can't" : 'can not', "could've": 'could have', "couldn't":	'could not', "couldn't've":	'could not have', "didn't":	'did not', "doesn't":	'does not', "don't":	'do not', "hadn't":	'had not', "hadn't've":	'had not have', "hasn't":	'has not', "haven't":	'have not', "he'd":	'he had', "he'd've":	'he would have', "he'll":	'he will', "he's":	'he is', "he'sn't":	'he is not', "how'd":	'how would', "how'll":	'how will', "how's":	'how is', "i'd":	'i would', "i'd've":	'i would have', "i'll":	'i will', "i'm":	'i am', "i've":	'i have', "i'ven't":	'i have not', "isn't":	'is not', "it'd":	'it had', "it'd've":	'it would have', "it'll":	'it will', "it's":	'it is', "it'sn't":	'it is not', "let's":	'let us', "ma'am":	'madam', "mightn't":	'might not', "mightn't've":	'might not have', "might've":	'might have', "mustn't":	'must not', "must've":	'must have', "needn't":	'need not', "not've":	'not have', "o'clock":	'of the clock', "oughtn't":	'ought not', "shan't":	'shall not', "she'd":	'she had or she would', "she'd've":	'she would have', "she'll":	'she shall or she will', "she's":	'she has or she is', "she'sn't":	'she has not or she is not', "should've":	'should have', "shouldn't":	'should not', "shouldn't've":	'should not have', "somebody'd":	'somebody had or somebody would', "somebody'd've":	'somebody would have', "somebody'dn't've":	'somebody would not have', "somebody'll":	'somebody shall or somebody will', "somebody's":	'somebody has or somebody is', "someone'd":	'someone had or someone would', "someone'd've":	'someone would have', "someone'll":	'someone shall or someone will', "someone's":	'someone has or someone is', "something'd":	'something had or something would', "something'd've":	'something would have', "something'll":	'something shall or something will', "something's":	'something has or something is', "that'll":	'that will', "that's":	'that has or that is', "there'd":	'there had or there would', "there'd've":	'there would have', "there're":	'there are', "there's":	'there has or there is', "they'd":	'they had or they would', "they'd've":	'they would have', "they'll":	'they shall or they will', "they're":	'they are', "they've":	'they have', "'twas":	'it was', "wasn't":	'was not', "we'd":	'we had or we would', "we'd've":	'we would have', "we'll":	'we will', "we're":	'we are', "we've":	'we have', "weren't":	'were not', "what'll":	'what shall or what will', "what're":	'what are', "what's":	'what has or what is or what does', "what've":	'what have', "when's":	'when has or when is', "where'd":	'where did', "where's":	'where has or where is', "where've":	'where have', "who'd":	'who would or who had', "who'd've":	'who would have', "who'll":	'who shall or who will', "who're":	'who are', "who's":	'who has or who is', "who've":	'who have', "why'll":	'why will', "why're":	'why are', "why's":	'why has or why is', "won't":	'will not', "won't've":	'will not have', "would've":	'would have', "wouldn't":	'would not', "wouldn't've":	'would not have', "y'all":	'you all', "y'all'll":	'you all will', "y'all'd've":	'you all would have', "y'all'dn't've":	'you all would not have', "you'd":	'you had or you would', "you'd've":	'you would have', "you'll":	'you shall or you will', "you're":	'you are', "you'ren't":	'you are not', "you've" :	'you have', "you'ven't": 'you have not'}
+
+def expand_contraction(text):
+    text    = text.split()
+    words   = [contractions_list[word] if word in contractions_list else word for word in text]
+    
+    return ' '.join(words)
